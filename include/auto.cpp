90c52e8b77120304c734b0acf5a175874036eaac
UnisefQw <NITRO5CY@outlook.com>
2024-02-11 20:58:50 +0000

    auto.cpp

--- include/auto.cpp
+++ include/auto.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cstdlib>
-using namespace std;
 
 
-bool passed,only;
+
+
+bool only,passed;
 int col,input;
 int auto_row;
 int temp;
@@ -42,6 +43,15 @@ bool botfourinaline(int grid[6][7], int row_test, int column, int player, int nu
     return false;
 }
 
+int rowposition(int grid[6][7],int column,int numrows){
+    for (int i=0;i<numrows;i++){
+        if (grid[i][column]==0){
+            return i;
+        }
+    }
+    return 0;
+}
+
 
 bool isFreeColumns(int numcols , int numrows, int grid[6][7] , int donotpick[7]){
     for (int i=0;i<numcols;i++){
@@ -53,81 +63,83 @@ bool isFreeColumns(int numcols , int numrows, int grid[6][7] , int donotpick[7])
 }
 
 int autoplayer(int grid[6][7], int numrows, const int numcols, int level){
-  if (level==3){
-    chosenrandom=-1;
-  } else {
-    chosenrandom=1;
-  }
-  int donotpick[numcols]={0};
-  temp=-1;
-  for (int col=0;col<numcols;col++){
-      passed=false;
-      for (int i=0;i<numrows;i++){
-          if (grid[i][col]==0 && passed==false){
-              auto_row=i;
-              passed=true;
-          }
-      }
-      if (passed==true){
-          grid[auto_row][col] = 2;
-          if (botfourinaline(grid, auto_row, col, 2, numrows, numcols)){
-            if (randwithrange(level*5)==chosenrandom){
-              cout<<"WOOPS"<<endl;
-            } else{
-              grid[auto_row][col] = 0;
-              cout<<"WIN FOUND "<<col<<endl;
-              return col;
-            }
-          }
-          
-          grid[auto_row][col] = 1;
-          if (botfourinaline(grid, auto_row, col, 1, numrows, numcols)){
-            if (randwithrange(level*5)==chosenrandom){
-              cout<<"WOOPS"<<endl;
-            } else{
-              grid[auto_row][col] = 0;
-              cout<<"GET BLOCKED"<<endl;
-              temp=col;
-            }
-          }
-          grid[auto_row][col] = 0;
-      }
-  }
-  if (temp!=-1){
-      return temp;
-  }
+    if (level==3){
+        chosenrandom=-1;
+    } else {
+        chosenrandom=1;
+    }
+    int donotpick[numcols]={0};
+    temp=-1;
+    for (int col=0;col<numcols;col++){
+        auto_row=rowposition(grid , col , numrows);
 
-  for (int col=0;col<numcols;col++){
-    passed=false;
-    for (int i=0;i<numrows-1;i++){
-      if (grid[i][col]==0 && grid[i+1][col]==0 && passed==false){
-        passed=true;
-        auto_row=i+1;
-      }    
-    }
-    if (passed==true){
-      grid[auto_row][col] = 1;
-      grid[auto_row-1][col] = 2;
-      if (botfourinaline(grid, auto_row, col, 1, numrows, numcols)){
-        donotpick[col] = 1;
-        cout<<"I WILL TRY NOT TO PICK "<<col+1<<endl;
-      }
-      grid[auto_row-1][col] = 0;
-      grid[auto_row][col] = 0;
-    }
-  }
-  while (true){
-    input=randwithrange(numcols+1);
+        if (auto_row==0){
+            continue;
+        }
+
+        grid[auto_row][col] = 2;
+        if (botfourinaline(grid, auto_row, col, 2, numrows, numcols)){
+            if (randwithrange(level*5)==chosenrandom){
+                std::cout << "WOOPS"<<std::endl;
+            } else{
+                grid[auto_row][col] = 0;
+                std::cout << "WIN FOUND " << col<<std::endl;
+                return col;
+            }
+        }
+        
+        grid[auto_row][col] = 1;
+        if (botfourinaline(grid, auto_row, col, 1, numrows, numcols)){
+            if (randwithrange(level*5)==chosenrandom){
+                std::cout << "WOOPS" << std::endl;
+            } else {
+                grid[auto_row][col] = 0;
+                std::cout << "GET BLOCKED" << std::endl;
+                temp=col;
+            }
+        }
+        grid[auto_row][col] = 0;
     
-    if (grid[numrows-1][input]==0 && donotpick[input]==0){
-      cout<<"Bot input column: "<<input+1<<", do not pick is: "<<donotpick[input]<<endl;
-      return input;
+    } 
+    if (temp!=-1){
+        return temp;
     }
-      
-    
-    if (isFreeColumns(numcols , numrows , grid , donotpick) && grid[numrows-1][input]==0){
-      cout<<"No good columns left, option chosen: " << input+1 << endl;
-      return input;
+
+    for (int col=0;col<numcols;col++){
+        
+        auto_row=rowposition(grid , col , numrows);
+        
+             
+        if (auto_row==-1 || auto_row==5){
+            continue;
+        }
+
+        auto_row++;
+
+        grid[auto_row][col] = 1;
+        grid[auto_row-1][col] = 2;
+        if (botfourinaline(grid, auto_row, col, 1, numrows, numcols)){
+            donotpick[col] = 1;
+            std::cout << "I WILL TRY NOT TO PICK " << col+1 << std::endl;
+        }
+
+        grid[auto_row-1][col] = 0;
+        grid[auto_row][col] = 0;     
+    } 
+
+
+    while (true){
+        input=randwithrange(numcols+1);
+        
+        if (grid[numrows-1][input]==0 && donotpick[input]==0){
+            std::cout<<"Bot input column: " << input+1 << ", do not pick is: " << donotpick[input] << std::endl;
+            return input;
+        }
+        
+        
+        if (isFreeColumns(numcols , numrows , grid , donotpick) && grid[numrows-1][input]==0){
+            std::cout<<"No prefered columns left, option chosen: " << input+1 << std::endl;   
+            return input;
+        }
     }
-  }
 }
